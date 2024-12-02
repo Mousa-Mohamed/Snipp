31cef0d313ef68c306164324e4cc4a0074bcc105
Mousa_Mohamed <mosapopo2016@gmail.com>
2024-12-02 07:58:59 +0200

    Some Edit

--- Snipp/project/src/Images.js
+++ Snipp/project/src/Images.js
@@ -1,21 +1,29 @@
+import imageOne from './Images/image_1.jpg.webp'
+import imageTwo from './Images/image_2.jpg.webp'
+import imageThree from './Images/image_3.jpg.webp'
+import imageFour from './Images/image_4.jpg.webp'
+import imageFive from './Images/image_5.jpg.webp'
+import imageSix from './Images/image_6.jpg.webp'
+import Use from './Images/Used.jpg'
+
 export default function Images() {
     return (
-        <section className="imagesProject">
-            <div className="img-1">
+        <section className="imagesProject d-flex justify-content-center align-items-center gap-4 p-4">
+            <div className="img-1 imgAll">
                 <div className="top">
                     <p>Racks</p>
                     <span>Website</span>
                 </div>
                 <div className="middle">
                     <img
-                        src="../Images/image_1.jpg.webp"
+                        src={imageOne}
                         alt="Rack"
                     />
                 </div>
                 <div className="bottom">
                     <div className="bottom-left">
                         <img
-                            src='../Images/Used.jpg'
+                            src={Use}
                             alt="Mousa"
                         />
                     </div>
@@ -25,117 +33,121 @@ export default function Images() {
                     </div>
                 </div>
             </div>
-            <div className="img-2">
-                <div className="top">
-                    <p>Racks</p>
-                    <span>Website</span>
-                </div>
-                <div className="middle">
-                    <img
-                        src="../Images/image_2.jpg.webp"
-                        alt="Rack"
-                    />
-                </div>
-                <div className="bottom">
-                    <div className="bottom-left">
+            <div className="middleImages1">
+                <div className="img-5 imgAll mb-4">
+                    <div className="top">
+                        <p>Racks</p>
+                        <span>Website</span>
+                    </div>
+                    <div className="middle">
                         <img
-                            src='../Images/Used.jpg'
-                            alt="Mousa"
+                            src={imageFive}
+                            alt="Rack"
                         />
                     </div>
-                    <div className="bottom-right">
-                        <p>Mousa</p>
-                        <span>Developer</span>
+                    <div className="bottom">
+                        <div className="bottom-left">
+                            <img
+                                src={Use}
+                                alt="Mousa"
+                            />
+                        </div>
+                        <div className="bottom-right">
+                            <p>Mousa</p>
+                            <span>Developer</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="img-6 imgAll">
+                    <div className="top">
+                        <p>Racks</p>
+                        <span>Website</span>
+                    </div>
+                    <div className="middle">
+                        <img
+                            src={imageSix}
+                            alt="Rack"
+                        />
+                    </div>
+                    <div className="bottom">
+                        <div className="bottom-left">
+                            <img
+                                src={Use}
+                                alt="Mousa"
+                            />
+                        </div>
+                        <div className="bottom-right">
+                            <p>Mousa</p>
+                            <span>Developer</span>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="img-3">
-                <div className="top">
-                    <p>Racks</p>
-                    <span>Website</span>
-                </div>
-                <div className="middle">
-                    <img
-                        src="../Images/image_3.jpg.webp"
-                        alt="Rack"
-                    />
-                </div>
-                <div className="bottom">
-                    <div className="bottom-left">
+            <div className="middleImages2">
+                <div className="img-4 imgAll mb-4">
+                    <div className="top">
+                        <p>Racks</p>
+                        <span>Website</span>
+                    </div>
+                    <div className="middle">
                         <img
-                            src='../Images/Used.jpg'
-                            alt="Mousa"
+                            src={imageFour}
+                            alt="Rack"
                         />
                     </div>
-                    <div className="bottom-right">
-                        <p>Mousa</p>
-                        <span>Developer</span>
+                    <div className="bottom">
+                        <div className="bottom-left">
+                            <img
+                                src={Use}
+                                alt="Mousa"
+                            />
+                        </div>
+                        <div className="bottom-right">
+                            <p>Mousa</p>
+                            <span>Developer</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="img-3 imgAll">
+                    <div className="top">
+                        <p>Racks</p>
+                        <span>Website</span>
+                    </div>
+                    <div className="middle">
+                        <img
+                            src={imageThree}
+                            alt="Rack"
+                        />
+                    </div>
+                    <div className="bottom">
+                        <div className="bottom-left">
+                            <img
+                                src={Use}
+                                alt="Mousa"
+                            />
+                        </div>
+                        <div className="bottom-right">
+                            <p>Mousa</p>
+                            <span>Developer</span>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="img-4">
+            <div className="img-2 imgAll">
                 <div className="top">
                     <p>Racks</p>
                     <span>Website</span>
                 </div>
                 <div className="middle">
                     <img
-                        src="../Images/image_4.jpg.webp"
+                        src={imageTwo}
                         alt="Rack"
                     />
                 </div>
                 <div className="bottom">
                     <div className="bottom-left">
                         <img
-                            src='../Images/Used.jpg'
-                            alt="Mousa"
-                        />
-                    </div>
-                    <div className="bottom-right">
-                        <p>Mousa</p>
-                        <span>Developer</span>
-                    </div>
-                </div>
-            </div>
-            <div className="img-5">
-                <div className="top">
-                    <p>Racks</p>
-                    <span>Website</span>
-                </div>
-                <div className="middle">
-                    <img
-                        src="../Images/image_5.jpg.webp"
-                        alt="Rack"
-                    />
-                </div>
-                <div className="bottom">
-                    <div className="bottom-left">
-                        <img
-                            src='../Images/Used.jpg'
-                            alt="Mousa"
-                        />
-                    </div>
-                    <div className="bottom-right">
-                        <p>Mousa</p>
-                        <span>Developer</span>
-                    </div>
-                </div>
-            </div>
-            <div className="img-6">
-                <div className="top">
-                    <p>Racks</p>
-                    <span>Website</span>
-                </div>
-                <div className="middle">
-                    <img
-                        src="../Images/image_6.jpg.webp"
-                        alt="Rack"
-                    />
-                </div>
-                <div className="bottom">
-                    <div className="bottom-left">
-                        <img
-                            src='../Images/Used.jpg'
+                            src={Use}
                             alt="Mousa"
                         />
                     </div>
